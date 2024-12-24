6b04669b8067d809ed0d727ffb94b62d7de1f42a
RakeShrestha <orakeshstha7@gmail.com>
2024-12-24 06:49:51 +0545

    Initial Commit

---
+++ UnitConversionApp/UnitConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionAppApp.swift
+//  UnitConversionApp
+//
+//  Created by Rakesh Shrestha on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
